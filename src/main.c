f4e639e531721143a1a3351199b62dadbf00553c
manuelacespede-ctrl <manu.cespede.tech@gmail.com>
2026-04-23 09:30:06 -0300

    leitura e armazenamento da segunda nota

--- src/main.c
+++ src/main.c
@@ -5,10 +5,8 @@ int main() {
     printf("--- O aplicativo só aceita duas notas por vez ---\n");
     printf ("Digite sua primeira nota: ");
     scanf("%f", &nota_1);
-    printf ("Pronto! Digite sua segunda nota: ");
+    printf ("digite sua segunda nota: ");
     scanf("%f", &nota_2);
-
-    media = nota_1 + nota_2 / 2;
 
     return 0;
 }

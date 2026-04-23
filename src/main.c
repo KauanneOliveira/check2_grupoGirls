4513637efd2a59530f725829413c5902e5d7a4c4
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2026-04-23 18:04:02 -0300

    ajustes finais no código

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-void calcular_derivada() {
+void calcular_derivada()
+{
     float a, b, c;
 
     printf("Digite o coeficiente de a ao quadrado (a): ");
@@ -20,12 +21,12 @@ void calcular_derivada() {
     printf("Derivada:           f'(x) = %.2fx + %.2f\n", derivada_a, derivada_b);
 }
 
-
-int main() {
+int main()
+{
     int opcao = 0;
     float nota_1, nota_2, media;
 
-    while(opcao != 6)
+    while (opcao != 6)
     {
         printf("\n\n");
         printf("==============================\n");
@@ -37,39 +38,61 @@ int main() {
         printf("4 - Exibir resultado\n");
         printf("5 - Calcular derivada\n");
         printf("6 - Sair\n");
-        
+
         printf("Digite a sua escolha: ");
         scanf("%d", &opcao);
 
         switch (opcao)
         {
         case 1:
-            printf("-- O aplicativo so aceita duas notas por vez ---\n");
-            printf ("Digite sua primeira nota: ");
-            scanf("%f", &nota_1);
-            printf ("Pronto! Digite sua segunda nota: ");
-            scanf("%f", &nota_2);
+            printf("--- O aplicativo so aceita duas notas por vez ---\n");
+
+            do
+            {
+                printf("Digite sua primeira nota: ");
+                scanf("%f", &nota_1);
+                if (nota_1 < 0 || nota_1 > 10)
+                {
+                    printf("[ERRO]: A nota deve ser positiva e menor ou igual a 10!\n");
+                }
+            } while (nota_1 < 0 || nota_1 > 10);
+
+            do
+            {
+                printf("Pronto! Digite sua segunda nota: ");
+                scanf("%f", &nota_2);
+                if (nota_2 < 0 || nota_2 > 10)
+                {
+                    printf("[ERRO]: A nota deve ser positiva e menor ou igual a 10!\n");
+                }
+            } while (nota_2 < 0 || nota_2 > 10);
             break;
-        case 2: 
+        case 2:
             media = (nota_1 + nota_2) / 2;
             printf("Media do Aluno: %f", media);
             break;
         case 3:
             int situacao;
-            if(media >= 6) {
+            if (media >= 6)
+            {
                 printf("Aluno Aprovado!");
                 situacao = 1;
-            } else {
+            }
+            else
+            {
                 printf("Aluno Reprovado!");
                 situacao = 0;
             }
             printf("\n");
             break;
         case 4:
-            if (situacao == 1){
-                printf("Media do Aluno: %f \n Situacao: Aprovado!", media);
-            } else {
-                printf("Media do Aluno: %f \n Situação: Reprovado!", media);
+            if (situacao == 1 || media >= 6)
+            {
+                printf("Media do Aluno: %f \nSituacao: Aprovado!", media);
+            }
+            else
+            {
+                printf("Media do Aluno: %f \nSituação: Reprovado!", media);
             }
             printf("\n");
             break;
@@ -79,7 +102,7 @@ int main() {
             break;
         default:
             printf("Digite a opcao correta!");
-        }       
+        }
     }
     return 0;
 }

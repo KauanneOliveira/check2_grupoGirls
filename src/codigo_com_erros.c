20b561666a90804a43a29b1a336292f3c1abe7c7
NayhelyEstela <Nayhely.estela.c.c@gmail.com>
2026-04-23 09:14:41 -0300

    Finalizando a correção

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -4,14 +4,15 @@ int main() {
     float nota1, nota2, media; // faltava ";"
 
     printf("Digite a primeira nota: ");
-    scanf("%f", &nota1); // faltava "&"
+    scanf("%f", &nota1); 
 
     printf("Digite a segunda nota: ");
     scanf("%f", &nota2); // faltava "&"
 
     media = (nota1 + nota2) / 2; // Para o funcionamento coreto da fórmula falta o () entre a nota1 e nota2
 
-    if (media = 6) {
+  
+    if (media >= 6) {  // Não pode estar com um sinal de atribuição "=" dever ser de comparação ">="
         printf("Aluno aprovado\n");
     } else {
         printf("Aluno reprovado\n");
